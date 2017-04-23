037a55fa334040b0b7f1d515c0546a62b9d9d5a3
Rafael Campos <campos.rb@hotmail.com>
2017-04-23 18:04:10 -0300

    remove node-module

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getTemp(city, country) {
 // put your api key here:
-  const API_KEY = ;
+  const API_KEY = 'dcb39fdbbd7b3496e9123e0b1b545e37';
   const url = `http://api.openweathermap.org/data/2.5/find?q=${city},${country}&units=metric&appid=${API_KEY}`;
 
   return axios.get(url).then((res) => {
